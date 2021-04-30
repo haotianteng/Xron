73193c4bd72d7746f8fbf2ff71915be15d448b60
haotianteng <havens.teng@gmail.com>
2021-04-29 23:37:10 -0400

    Add a chunk preparation scripts and update valid error

---
+++ xron/utils/prepare_chunk.py
@@ -0,0 +1,100 @@
+"""
+Created on Wed Apr 28 23:03:36 2021
+
+@author: Haotian Teng
+"""
+
+import os
+import sys
+import h5py
+import numpy as np
+import argparse
+from tqdm import tqdm
+from scipy.signal import find_peaks
+
+def fast5_iter(fast5_dir,mode = 'r'):
+    for (dirpath, dirnames, filenames) in os.walk(fast5_dir+'/'):
+        for filename in filenames:
+            if not filename.endswith('fast5'):
+                continue
+            abs_path = os.path.join(dirpath,filename)
+            try:
+                root = h5py.File(abs_path,mode = mode)
+            except OSError as e:
+                print("Reading %s failed due to %s."%(abs_path,e))
+                continue
+            for read_id in root:
+                read_h = root[read_id]['Raw']
+                if 'Signal_Old' in read_h:
+                    signal = np.asarray(read_h[('Signal_Old')],dtype = np.float32)
+                else:
+                    signal = np.asarray(read_h[('Signal')],dtype = np.float32)
+                read_id = read_h.attrs['read_id']
+                yield read_h,signal,abs_path,read_id.decode("utf-8")
+
+def med_mad(x, factor=1.4826):
+    """
+    Calculate signal median and median absolute deviation
+    Same method used in Bonito Basecaller.
+    """
+    med = np.median(x)
+    mad = np.median(np.absolute(x - med)) * factor
+    return med, mad
+
+
+def norm_by_noisiest_section(signal, samples=100, threshold=6.0):
+    """
+    Normalise using the medmad from the longest continuous region where the
+    noise is above some threshold relative to the std of the full signal.
+    """
+    threshold = signal.std() / threshold
+    noise = np.ones(signal.shape)
+
+    for idx in np.arange(signal.shape[0] // samples):
+        window = slice(idx * samples, (idx + 1) * samples)
+        noise[window] = np.where(signal[window].std() > threshold, 1, 0)
+
+    # start and end low for peak finding
+    noise[0] = 0; noise[-1] = 0
+    peaks, info = find_peaks(noise, width=(None, None))
+
+    if len(peaks):
+        widest = np.argmax(info['widths'])
+        med, mad = med_mad(signal[info['left_bases'][widest]: info['right_bases'][widest]])
+    else:
+        med, mad = med_mad(signal)
+    return (signal - med) / mad
+
+def extract(args):
+    iterator = fast5_iter(args.input_fast5,mode = 'r')
+    meta_info = []
+    chunks = []
+    for read_h,signal,fast5_f,read_id in tqdm(iterator):
+        read_len = len(signal)
+        signal = norm_by_noisiest_section(signal).astype(np.float16)
+        current_chunks = np.split(signal,np.arange(0,read_len,args.chunk_len))[1:]
+        last_chunk = current_chunks[-1]
+        current_chunks[-1]= np.pad(last_chunk,(0,args.chunk_len-len(last_chunk)),'constant',constant_values = (0,0))
+        chunks += current_chunks
+        meta_info += [(fast5_f,read_id)]*len(current_chunks)
+    chunks = np.stack(chunks,axis = 0)
+    np.savetxt(os.path.join(args.output,'meta.csv'),meta_info,fmt="%s")
+    np.save(os.path.join(args.output,'chunks.npy'),chunks)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(prog='xron',
+                                     description='A Unsupervised Nanopore basecaller.')
+    parser.add_argument('-i', 
+                        '--input_fast5', 
+                        required = True,
+                        help="File path or Folder path to the fast5 file.")
+    parser.add_argument('-o',
+                        '--output',
+                        required = True,
+                        help="Output folder.")
+    parser.add_argument('--chunk_len',
+                        default = 4000,
+                        type=int,
+                        help="The lenght of the segment in chunk.")
+    FLAGS = parser.parse_args(sys.argv[1:])
+    extract(FLAGS)
--- xron/xron_train_variational.py
+++ xron/xron_train_variational.py
@@ -13,6 +13,7 @@ from torch.utils.data.dataloader import DataLoader
 from xron.xron_input import Dataset, ToTensor
 from xron.xron_train_base import Trainer, DeviceDataLoader, load_config
 from xron.xron_model import REVCNN,DECODER_CONFIG,CRNN
+from xron.xron_label import MetricAligner
 from torch.distributions.one_hot_categorical import OneHotCategorical as OHC
 
 class VAETrainer(Trainer):
@@ -21,6 +22,7 @@ class VAETrainer(Trainer):
                  encoder:CRNN,
                  decoder:REVCNN,
                  config:DECODER_CONFIG,
+                 aligner:MetricAligner,
                  device:str = None,
                  eval_dataloader:DataLoader = None):
         """
@@ -51,14 +53,23 @@ class VAETrainer(Trainer):
                          eval_dataloader = eval_dataloader)
         self.encoder = encoder
         self.decoder = decoder
+        self.aligner = aligner
         params = [encoder.parameters(),decoder.parameters()]
         self.parameters = chain(*params)
+        self.decay = config.TRAIN["decay"]
         self.global_step = 0
         self.awake = False
+        self.score_average = 0
         self.wake_sleep_cycle = config.TRAIN['Sleep_Wake_Cycle']
+        self.records = {'rc_losses':[],
+                        'rc_losses_encoder':[],
+                        'entropy_losses':[],
+                        'alignment_score':[],
+                        'valid_alignment':[]}
     def train(self,epoches,optimizer,save_cycle,save_folder):
         self.save_folder = save_folder
         self._save_config()
+        records = self.records
         for epoch_i in range(epoches):
             for i_batch, batch in enumerate(self.train_ds):
                 if (self.global_step+1)%self.wake_sleep_cycle == 0:
@@ -67,19 +78,30 @@ class VAETrainer(Trainer):
                         param.requires_grad = self.awake
                     for param in self.decoder.parameters():
                         param.requires_grad = not self.awake
-                loss = self.train_step(batch,phase = self.awake)
+                losses = self.train_step(batch,phase = self.awake)
+                loss = sum(losses)
                 optimizer.zero_grad()
                 loss.backward()
                 if (self.global_step+1)%save_cycle==0:
                     self.save()
                     eval_i,valid_batch = next(enumerate(self.eval_ds))
                     valid_error,valid_perm = self.valid_step(valid_batch)
-                    print("Epoch %d Batch %d, loss %f, valid_error %f perm:%s"%(epoch_i, i_batch, loss, valid_error, valid_perm))
+                    records['entropy_losses'].append(losses[0].detach().cpu().numpy()[()])
+                    records['valid_alignment'].append(valid_error)
+                    print(losses)
+                    print(valid_error)
+                    if self.awake:
+                        print("Epoch %d Batch %d, entropy_loss %f, rc_loss %f, valid_error %f, perm:%s, alignment_loss %f"%(epoch_i, i_batch, losses[0], losses[1], valid_error, valid_perm, losses[2]))
+                        records['alignment_score'].append(losses[2].detach().cpu().numpy()[()])
+                        records['rc_losses_encoder'].append(losses[1].detach().cpu().numpy()[()])
+                    else:
+                        print("Epoch %d Batch %d, entropy_loss %f, rc_loss %f, valid_error %f, perm:%s"%(epoch_i, i_batch, losses[0], losses[1], valid_error, valid_perm))
+                        records['rc_losses'].append(losses[1].detach().cpu().numpy()[()])
+                    self._update_records()
                 torch.nn.utils.clip_grad_norm_(self.parameters, 
                                                max_norm=self.grad_norm)
                 optimizer.step()
                 self.global_step +=1
-    
         
     def train_step(self,batch,phase = 0):
         encoder = self.encoder
@@ -93,26 +115,42 @@ class VAETrainer(Trainer):
         entropy_loss = decoder.entropy_loss(logprob.permute([1,2,0]),sampling.max(dim = 1)[1])
         if phase == 0: #Phase 0 when we update the decoder
             rc_loss = torch.mean(mse_loss)
-            return entropy_loss+rc_loss
+            return entropy_loss,rc_loss
         else: #Phase 1 when we update the encoder.
+            raw_seq = torch.argmax(sampling,dim = 1)
             sample_logprob = torch.sum(logprob.permute([1,2,0])*sampling,axis = (1,2))
             rc_loss = torch.mean(torch.mean(mse_loss,axis = (1,2))*sample_logprob)
-            return entropy_loss+rc_loss
-                                       
+            
+            # ### Train with aligner
+            # identities,perm = self.aligner.permute_align(raw_seq.cpu().detach().numpy())
+            # score = np.mean(identities,axis = 1)
+            # best_perm = np.argmax(score)
+            # score = torch.from_numpy(identities[best_perm]-self.score_average).to(self.device)
+            # self.score_average  = self.score_average * self.decay + (1-self.decay)* np.mean(identities[best_perm])
+            # return entropy_loss,rc_loss, torch.mean(-score*sample_logprob)
+            
+            return entropy_loss, rc_loss,0
+
     def valid_step(self,batch):
         net = self.encoder
         signal_batch = batch['signal']
         out = net.forward(signal_batch)
-        seq = batch['seq']
-        seq_len = batch['seq_len'].view(-1)
-        errors,perms = net.permute_error(out,
-                       seq,
-                       seq_len,
-                       alphabet = 'N' + self.config.CTC['alphabeta'],
-                       beam_size = self.config.CTC['beam_size'],
-                       beam_cut_threshold = self.config.CTC['beam_cut_threshold'])
-        best_perm = np.argmin(errors)
-        return errors[best_perm],perms[best_perm]
+        if 'seq' in batch.keys():
+            seq = batch['seq']
+            seq_len = batch['seq_len'].view(-1)
+            errors,perms = net.permute_error(out,
+                           seq,
+                           seq_len,
+                           alphabet = 'N' + self.config.CTC['alphabeta'],
+                           beam_size = self.config.CTC['beam_size'],
+                           beam_cut_threshold = self.config.CTC['beam_cut_threshold'])
+            best_perm = np.argmin(errors)
+        else:
+            out_seq = torch.argmax(out,dim = 2)
+            identities,perms = self.aligner.permute_align(out_seq.cpu().detach().numpy())
+            score = np.mean(identities,axis = 1)
+            best_perm = np.argmax(score)
+        return score[best_perm],perms[best_perm]
     
 def main(args):
     class CTC_CONFIG(DECODER_CONFIG):
@@ -129,9 +167,10 @@ def main(args):
         
     config = TRAIN_CONFIG()
     print("Read chunks and sequence.")
-    chunks = np.load(args.chunks)
-    reference = np.load(args.seq)
-    ref_len = np.load(args.seq_len)
+    chunks = np.load(args.chunks,allow_pickle = True)
+    print(chunks.shape)
+    reference = np.load(args.seq) if args.seq else None
+    ref_len = np.load(args.seq_len) if args.seq_len else None
     print("Construct and load the model.")
     model_f = args.model_folder
     dataset = Dataset(chunks,seq = reference,seq_len = ref_len,transform = transforms.Compose([ToTensor()]))
@@ -144,7 +183,8 @@ def main(args):
         config = config_old
     encoder = CRNN(config)
     decoder = REVCNN(config)
-    t = VAETrainer(loader,encoder,decoder,config)
+    aligner = MetricAligner(args.reference)
+    t = VAETrainer(loader,encoder,decoder,config,aligner)
     if args.retrain:
         t.load(model_f)
     lr = args.lr
@@ -153,6 +193,7 @@ def main(args):
     COUNT_CYCLE = args.report
     print("Begin training the model.")
     t.train(epoches,optimizer,COUNT_CYCLE,model_f)
+    
         
         
 if __name__ == "__main__":
@@ -162,9 +203,11 @@ if __name__ == "__main__":
                         help = "The .npy file contain chunks.")
     parser.add_argument('-o', '--model_folder', required = True,
                         help = "The folder to save folder at.")
-    parser.add_argument('--seq', required = True,
+    parser.add_argument('-r', '--reference', required = True,
+                        help = "The reference fastq file.")
+    parser.add_argument('--seq', default = None,
                         help="The .npy file contain the sequence.")
-    parser.add_argument('--seq_len', required = True,
+    parser.add_argument('--seq_len', default = None,
                         help="The .npy file contain the sueqnece length.")
     parser.add_argument('--device', default = 'cuda',
                         help="The device used for training, can be cpu or cuda.")
