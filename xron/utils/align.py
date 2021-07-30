b331f15d0dac9d558b5150bf10270ad0b1435b75
Haotian Teng <havens.teng@gmail.com>
2021-07-29 20:40:12 -0400

    Alignment with reference

---
+++ xron/utils/align.py
@@ -0,0 +1,185 @@
+"""
+Created on Sun Feb 21 05:22:32 2021
+
+@author: Haotian Teng
+"""
+# from bwapy import BwaAligner
+# from bwapy.libbwa import Alignment
+import sys
+if 'bwapy' in sys.modules:
+    raise ImportError("Loading bwapy again will cause error, please restart " \
+                      "the process.")
+import bwapy
+import re
+import numpy as np
+from numpy import ndarray
+from typing import List, Optional
+from xron.utils.seq_op import raw2seq
+from itertools import permutations
+from collections import defaultdict
+
+
+class MetricAligner(bwapy.BwaAligner):
+    def __init__(self,
+                 reference: str,
+                 min_len: int = 7,
+                 options:str = None
+                 ):
+        """
+        The wrapper aligner class of bwa aligner, measure the quality for 
+        batch of output sequences from a basecall.
+
+        Parameters
+        ----------
+        reference : str
+            File path of a reference fasta/fastq file.
+        min_len : int, optional
+            The minimum sequence length to be aligned. The default is 7.
+
+        """
+        # options = '-x ont2d'
+        if not options:
+            options = '-A 1 -B 1 -k %d -O 3 -T 0'%(min_len)
+        super().__init__(index = reference,options = options)
+        self.reference = defaultdict(list)
+        with open(reference,'r') as f:
+            for line in f:
+                if line.startswith(">"):
+                    key = line.strip().split(' ')[0][1:]
+                else:
+                    self.reference[key].append(line.strip())
+    
+    def permute_align(self,
+                      raw: ndarray,
+                      alphabet:str = "NACGT",
+                      mod_base:dict = {},
+                      min_len:int = 5,
+                      permute:bool = True
+                      )->ndarray:
+        """
+        Align the input sequence with best permutation.
+
+        Parameters
+        ----------
+        raw : A [N,L] ndarray gives the raw ctc sequence.
+            where N is the number of sequences and L is the length of each
+            sequence, where 0 alaways mean the blank symbol.
+        permute: List[str] Optional
+            A permute list, default is None, will try every possible permutation.
+        mod_base: Dict Optional
+            A dict mapping the modification base to the original base.
+        Returns
+        -------
+        ndarray
+            A arra with same length of the sequences, gives the identity scores.
+
+        """
+        
+        ab = alphabet[1:]
+        seqs = raw2seq(raw)
+        len_score = np.asarray([len(seq) for seq in seqs])-min_len
+        len_score[len_score<0] = 0
+        scores = []
+        perms = []
+        if permute:
+            for perm in permutations(ab):
+                perm_seqs = ["".join([perm[x-1] for x in seq]) for seq in seqs]
+                identities = self.align(perm_seqs)
+                identities = identities * len_score
+                scores.append(identities)
+                perms.append(perm)
+        else:
+            seqs = ["".join([ab[x-1] for x in seq]) for seq in seqs]
+            identities = self.align(seqs)
+            identities = identities * len_score
+            scores.append(identities)
+            perms.append([x for x in ab])
+        return scores, perms
+        
+        
+    def align(self,
+              sequences: List[str]
+              ) -> ndarray:
+        """
+        Get the align score given a batch of sequences.
+
+        Parameters
+        ----------
+        sequences : List[str]
+            A list of the sequences.
+        Returns
+        -------
+        ndarray
+            A array with same length of the sequences, gives the identity
+            scores.
+
+        """
+        identities = []
+        for seq in sequences:
+            hits = self.align_seq(seq)
+            identity = self._match_score(hits[0])/len(seq) if len(hits)>0 else 0  #Only use the first hit.
+            identities.append(identity)
+        return np.array(identities)
+    
+    def _match_score(self, alignment: Optional[bwapy.libbwa.Alignment]) -> int:
+        """
+        Get the match score given a cigar string.
+    
+        Parameters
+        ----------
+        alignment : Optional[Alignment]
+            A alignment instance from the bwapy mapping result.
+    
+        Returns
+        -------
+        int
+            The nubmer of matched bases.
+    
+        """
+        cigar_s = alignment.cigar
+        n_match = self._count_cigar(cigar_s,'M')
+        return n_match - alignment.NM
+
+    def ref_seq(self,seq:str):
+        """
+        Return the reference sequence of the first hit of the alignment
+
+        Parameters
+        ----------
+        seq : str
+            The query sequence.
+
+        Returns
+        -------
+        ref_seq: Related reference sequence 
+        ref_idx: Index reference (index relatively to the query sequence).
+
+        """
+        hits = self.align_seq(seq)
+        if not hits:
+            return None,None
+        cigar = hits[0]
+        deletions = self._count_cigar(cigar,'D')
+        insertions = self._count_cigar(cigar,'I')
+        matchs = self._count_cigar(cigar,'M')
+        clips = re.findall(r'(\d+)S', cigar)
+
+    def _count_cigar(self,cigar_string,c = 'M'):
+        return sum([int(x) for x in re.findall(r'(\d+)%s'%(c),cigar_string)])
+        
+if __name__ == "__main__":
+    reference_f = '/home/heavens/twilight_hdd1/m6A_Nanopore/references.fasta'
+    test_seqs = ['AAGAUUGUGUUGGUCAGUACUCGGUCGACGGGGUGAUUCCUUCAGCCCAGCCUGAUGAAUGCCGACACCUACCGGCUUUUAAGACGCCCAGGUACCAACGAUCAUCGUCGACCGUAAGU',
+                'AUAGAUUGUGUUUGUUAGUCGCUUGGUACGCCGAGCUGAAUGAUUGUGGAUAUCGCGAAGCAGGUUGAGCGCAACCCUGGACGACCGUAUUUCGGCGUCAAUAAGCGUCGUGAAGGCAGGUGAAUAUGAGCAUGGCGUUGGGGCUUUCCGAUAUCAAAGGCGCAGAUGAGCGGUCUGGUAUACCGUGGUUUGUGACUGGCAACGGCUUUAACGUUGGCGAUACGCUGCGUUAUCUGCAGGCAAUUGAGCGGCUGGAGAAAAACUGGCUGUCGAUCCGUCCGAACCGGGCGCGAAAUGCCGAAAGUCGAGCGUCCAGCAGGCGUGGCAUAAUGGAUAAUAAAACUGCCCAGCGCGUCGCGAAGAUGAUGAUGUUAAAGAGAUCCGUUUGGAUGAUUGAAGAACUGCGUGUCAGUAUUCGCCCAACGGUUACGGUGCCUUAUCCUAUCUCCGAUAAGCGUAUUUUACAGGCCAUGGAUCAGAUUACGGCCUAAAGCCAGGCGACGAUGAGCUAAAGAGAUCCAGUACAAGUUCGGAACAAAAAGGAAAUUAUGAAUACAACGCCAUUCCAGCGCUUUAAUGAAACCAUUGCUGGCGGUGAAUACACCCAAUGACAGUGGCUGGCGCAGCGUGAUUUUUCCGGCGAACAACCAUCAUCGGAUUCGCGACCGGCGGGCAAUGCGGUGAUCCCACAUCGACGCGGCCUGUCGCAUCGCCAAGCGCAGGCGUUCCACUGAUUGCGGCGGUAUAAUGGGUCACGCCUUCGACGCCGUUUUUGCGCCGUUGAUUGCCCGGCAUCCCGCGCUACCACACUAUCUGCCAACCGGACGCGCUGAAGCCAUCGAUCUCGCGGAUCGCGAACCAGUCUGGCAUAUUCCGGCUGAGAAAUCUGGCUGGAAGGUCGGUCAACUAUUGCGGCGAAAAUGCCCGUCACCUGCGUCAAUCCGCCAGGCGAAAGAAAACAUUAACGGCUAUCGUUGCGAAGACCCCACCAUGCAGCAGCGCACUAUCGCGUUAUUCCGGCGCGUGACAAACGACGACACCGAUGCGUGCUGGCUGAGUUUUUCCUGGUUUGUUCCAGUACUGCGCCACCUUAACGGCGAUACACGUUUUGCCUGUGUUGAAGAGGGCAUGGGACGGUGGAGCGUUAUCUGUACCGAGAUUGCCGGCGAGCUGCCUGCGUGAUGAAACAGGGUAAGCACCGCGCGGUAAAGAUUUUAUUUCACGUUGGAUAUCCCCACACGCGAUUAUUAGAAACGCAUUGGCGGUAUUAGCAGGCAGAUACGACGCUGCGCAGUGCAAUUAGAACAGAGAGCGUUCGCGAAAAAAUCCCGUCUGCGGGUGAACGGGCAAGUGCGACCACCAUAUCUCAUAAAUUUAAUGAAUAUUUAUAAAAGCAAAAUCGUUGUCGCUCACGGUUUCAUUCAGGACGCGCUAUGGGCGGCAAGUAUUCCGGCCUGCAAAUUGGUAUUUUACUGGUUAAGU',
+                'AGACUUGUGUUUGGCGGUCGCUUCUAAACCAGCGUAAAAGA',
+                'CAGAUAGAUAGUGUUUGUUAGUCGCUUUGGUGCGAAACUCCACUUAUCUACUUACUACGGUCAAAUUAACGAUCAAGCUUUAAGUACGUCGCCAGAUUGUCUGGUUUAACCCUCAAGUGCGGGACAAUUAUUGGGACGAAUACUACGAGUACGGUCGUGUGGCCAUCUCCACCGUCCCCCGCUGUAAUGCUUUUUUUACUCGUCUUAAGGAUAGGUAUCAUCUGACUAGUACCGGCGUGCAAAAGCUUUUUCCAGGAAAGUAGUCAGGAUGAGGCUGGUGAACUGUUACGAAAGAUGAAGGUAAGACCUCAUCCUACAUUUCACAGGCAAGCCAUUAAAGUGCACUGUCUCGUGGCGAUUGCAACUUCGAUCGGCAUCUGAUCAUUGGCCAGCCCUUAGCCGUGUUUCGGUGUGAAGCAGCGUGGCGGGAAUCGCCAUCAUGAUCCCCGCAUGUAGGGCCCUAUCGGAAUUACACCGUCCUAAUGGUUUUUGAAUUGCGGUGGCGCCAGAAAAGA',
+                'AGAUUGUGUUUGUUAGUUGCUUAACAAAAUGGUACAGAGAUGAUAGAAAGGCCUGGAGCCGUCUUCCGGGAAUCCAGAACAGAAUCAUCAACAUUUCCACGAAUCUCAAAGAGCUUAUUGAUUUUGGGCUGAGGCAGCUUGGAAGAAAGCAAUCUCCCAGGCAAAAGCAGCUGUUUUUUUAUCCGUCGAACGGUACUGCACUGCUGUCAUGGGCCGACAGGUUCCAGUAGCUCCUUACCAUGCGAACCGGAUCUACCAGUUUGCCAUCUACUCUGACAUUUUGAAGCUCAGGUGCACCAUUUAUUACACUAACUGUGCAGAUUACCAAUCCAAACUGCUUUCAAUUCGGACACAAAAAAAAGGUUUUACAGGUGCCAGCGAGGAUAAGGCUGGUUUAGUCGAACAAGCCAAUGGAGCAUACACUCUUAUGGACGGCUCCGCCUCCCAAAGGCAAGAAAUGUUGUCUUGCUACUGACAGCGGUUCACCACAACGGCUUGGAGAAUCCUGAACAUAAACGAACAUCAGACGUAUUGUUUAUUUGUGCCACUACAGAAAACCUAGGUCUGCAACUGAAAACAUUUUUGCAGAAUCCCAAUGACCAUUUUAACCAUGUUGUAGAACGAUCGCUUAAAGAAAAGGGUGGAUCUGACGACGUUUUAUUUAGGAAAGAAGCAGAACGAAUUAAGAAAAAUCUGAGUGUGCACAUAGAUGUCUAUAAUGCGCUUAUUCAUUCUGCGAAAUUUGAAAUGUGGGACAGCUGAAAUCAAAUGUCCAGUUAGUUUGUGCACACAGGGUUUUUGUUCCAACCUUGACGGACCGAGGUUAAUGAAAUGACUGUUCGGGAUCUGCCGGAUGAAAUCAAGCAAGAAUGGAUGUCCAGCAGCAAAAUACUGCAAAGGAGCAAAGCCAUUUGAAUGUCAUAAUUUUUUCGAUCAUCUCCGAUUAAGCGGAAGAUGAAACAACCGAAUAUUGACGAGGACCCUUUUCAUUUAACUUGUAUCAUAUUGCGAUCGAAGAAAAGGAG',
+                'AGAUUGUGUUUGUUAGUUGCUUAACAAAAUGGUACAGAGAUGAUAGAAAGGCCUGGAGCCGUCUUCCGGGAAUCCAGAACAGAAUCAUCAACAUUUCCACGAAUCUCAAAGAGCUUAUUGAUUUUGGGCUGAGGCAGCUUGGAAGAAAGCAAUCUCCCAGGCAAAAGCAGCUGUUUUUUUAUCCGUCGAACGGUACUGCACUGCUGUCAUGGGCCGACAGGUUCCAGUAGCUCCUUACCAUGCGAACCGGAUCUACCAGUUUGCCAUCUACUCUGACAUUUUGAAGCUCAGGUGCACCAUUUAUUACACUAACUGUGCAGAUUACCAAUCCAAACUGCUUUCAAUUCGGACACAAAAAAAAGGUUUUACAGGUGCCAGCGAGGAUAAGGCUGGUUUAGUCGAACAAGCCAAUGGAGCAUACACUCUUAUGGACGGCUCCGCCUCCCAAAGGCAAGAAAUGUUGUCUUGCUACUGACAGCGGUUCACCACAACGGCUUGGAGAAUCCUGAACAUAAACGAACAUCAGACGUAUUGUUUAUUUGUGCCACUACAGAAAACCUAGGUCUGCAACUGAAAACAUUUUUGCAGAAUCCCAAUGACCAUUUUAACCAUGUUGUAGAACGAUCGCUUAAAGAAAAGGGUGGAUCUGACGACGUUUUAUUUAGGAAAGAAGCAGAACGAAUUAAGAAAAAUCUGAGUGUGCACAUAGAUGUCUAUAAUGCGCUUAUUCAUUCUGCGAAAUUUGAAAUGUGGGACAGCUGAAAUCAAAUGUCCAGUUAGUUUGUGCACACAGGGUUUUUGUUCCAACCUUGACGGACCGAGGUUAAUGAAAUGACUGUUCGGGAUCUGCCGGAUGAAAUCAAGCAAGAAUGGAUGUCCAGCAGCAAAAUACUGCAAAGGAGCAAAGCCAUUUGAAUGUCAUAAUUUUUUCGAUCAUCUCCGAUUAAGCGGAAGAUGAAACAACCGAAUAUUGACGAGGACCCUUUUCAUUUAACUUGUAUCAUAUUGCGAUCGAAGAAAAGGAG',
+                'AAAGTAAGCCTGCTCGCTAAGTAGTCTTGCCCGCTCGGTGCGCAGTTCAATCGCTTTCAACAG']
+    
+    aln = MetricAligner(reference_f,options = '-x ont2d -A 1 -B 0')
+    scores = aln.align(test_seqs)
+    hits = aln.align_seq(test_seqs[1]) #The position is the postiive start position in reference genome even for reverse complement alignment.
+    print(scores)
+    print(hits)
+    
--- xron/utils/prepare_chunk.py
+++ xron/utils/prepare_chunk.py
@@ -11,6 +11,18 @@ import numpy as np
 import argparse
 from tqdm import tqdm
 from scipy.signal import find_peaks
+from xron.utils.align import MetricAligner
+from Bio.Seq import Seq
+alt_map = {'ins':'0','M':'A','U':'T'}
+complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'} 
+
+def reverse_complement(seq):    
+    return str(Seq(seq).reverse_complement())
+
+def clean_repr(seq):
+    for k,v in alt_map.items():
+        seq = seq.replace(k,v)
+    return seq
 
 def fast5_iter(fast5_dir,mode = 'r'):
     for (dirpath, dirnames, filenames) in os.walk(fast5_dir+'/'):
@@ -42,7 +54,8 @@ def med_mad(x, factor=1.4826):
 def norm_by_noisiest_section(signal, samples=100, threshold=6.0):
     """
     Normalise using the medmad from the longest continuous region where the
-    noise is above some threshold relative to the std of the full signal.
+    noise is above some threshold relative to the std of the full signal.This
+    function is borrowed from Bonito Basecaller.
     """
     threshold = signal.std() / threshold
     noise = np.ones(signal.shape)
@@ -62,13 +75,41 @@ def norm_by_noisiest_section(signal, samples=100, threshold=6.0):
         med, mad = med_mad(signal)
     return (signal - med) / mad
 
+def retrive_seq(seq_h,event_stride):
+    moves = np.asarray(seq_h['BaseCalled_template']['Move'])
+    seq = np.asarray(seq_h['BaseCalled_template']['Fastq']).tobytes().decode('utf-8').split('\n')[1]
+    pos = np.repeat(np.cumsum(moves)-1,repeats = event_stride)
+    return seq,pos
+    
+
 def extract(args):
     iterator = fast5_iter(args.input_fast5,mode = 'r')
+    aligner = MetricAligner(args.reference,options = '-x ont2d')
     meta_info = []
     chunks = []
+    seqs = []
     for read_h,signal,fast5_f,read_id in tqdm(iterator):
         read_len = len(signal)
         signal = norm_by_noisiest_section(signal).astype(np.float16)
+        if args.mode == "rna" or args.mode == "rna-meth":
+            signal = signal[::-1]
+        if args.extract_seq:
+            seq, pos = retrive_seq(read_h['Analyses/Basecall_1D_%s'%(args.basecall_entry)],
+                                   args.stride)
+            seq = clean_repr(seq) #M->A, U->T
+            hit = aligner.align_seq(seq)
+            if not hit:
+                continue
+            hit = hit[0]
+            if hit.orient == '-':
+                seq = reverse_complement(seq)
+                
+            start = read_h['Analyses/Segmentation_001/Summary/segmentation'].attrs['first_sample_template']
+            signal = signal[start:]
+            read_len -= start
+            for x in np.arange(0,read_len,args.chunk_len):
+                s,e = pos[x:x+args.chunk_len][[1,-1]]
+                seqs.append(seq[s:e])
         current_chunks = np.split(signal,np.arange(0,read_len,args.chunk_len))[1:]
         last_chunk = current_chunks[-1]
         current_chunks[-1]= np.pad(last_chunk,(0,args.chunk_len-len(last_chunk)),'constant',constant_values = (0,0))
@@ -77,6 +118,13 @@ def extract(args):
     chunks = np.stack(chunks,axis = 0)
     np.savetxt(os.path.join(args.output,'meta.csv'),meta_info,fmt="%s")
     np.save(os.path.join(args.output,'chunks.npy'),chunks)
+    if args.extract_seq:
+        seq_lens = [len(i) for i in seqs]
+        pad = max(seq_lens)
+        seq_chunks = np.array([np.pad(i, ((0,pad-j),(0,0))) for i,j in zip(seqs,seq_lens)])
+        seq_lens = np.array(seq_lens)
+        np.save(os.path.join(args.output,'seqs.npy'),seq_chunks)
+        np.save(os.path.join(args.output,'seq_lens.npy'),seq_lens)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='xron',
@@ -93,5 +141,32 @@ if __name__ == "__main__":
                         default = 4000,
                         type=int,
                         help="The lenght of the segment in chunk.")
+    parser.add_argument("--extract_seq",
+                        action = "store_true",
+                        help = "If the sequence information is going to be\
+                            extracted.")
+    parser.add_argument('--basecall_entry',
+                        default = "000",
+                        help="The entry number in /Analysis/ to look into, for\
+                            example 000 means looking for BaseCall_1D_000.")
+    parser.add_argument('--stride',
+                        default = 10,
+                        type = int,
+                        help = "The length of stride used in basecall model,\
+                        for guppy RNA fast, this number is 12, for guppy RNA\
+                        hac model, this number is 10, for xron this number is\
+                        5."
+                        )
+    parser.add_argument('--reference',
+                        default = None,
+                        help = "The reference genome, it's required when\
+                        extract_seq is set to True")
+    parser.add_argument('--mode',
+                        default = "rna",
+                        help = "Can be one of this mode: rna, dna, rna-meth")
     FLAGS = parser.parse_args(sys.argv[1:])
+    if FLAGS.extract_seq:
+        if not FLAGS.reference:
+            raise ValueError("Reference genome is required when extract the \
+                             sequence.")
     extract(FLAGS)
